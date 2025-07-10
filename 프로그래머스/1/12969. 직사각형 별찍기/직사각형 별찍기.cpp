897f28111230dbb5879429f9f39ba6c44f1bddb9
유운선 <118857354+dbdnstjs@users.noreply.github.com>
2025-07-10 09:24:43 +0900

    [level 1] Title: 직사각형 별찍기, Time: 4.62 ms, Memory: 4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    int n,m,i,j;
+    scanf("%d %d",&n,&m);
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++) printf("*");
+        printf("\n");
+    }
+    
+    return 0;
+}
