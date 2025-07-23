231a08590e95200f654546c4654cb134891dcc75
유운선 <118857354+dbdnstjs@users.noreply.github.com>
2025-07-23 09:15:17 +0900

    [level 2] Title: n^2 배열 자르기, Time: 38.56 ms, Memory: 22.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define MAX(x,y) (x>y?x:y)
+
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+    
+    for(int long long i=left;i<=right;i++) answer.push_back(MAX(i/n,i%n)+1);
+        
+    return answer;
+}
